6e791dbd32509f09494932f9d893bded0170020e
aslam jm <aslamjm@aslams-MacBook-Pro.local>
2022-04-05 16:06:24 +0530

    heyyo routes fixed

--- utils/requests.js
+++ utils/requests.js
@@ -7,34 +7,34 @@ export default {
   },
   fetchToprated: {
     title: "Top Rated",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   },
   fetchAction: {
     title: "Action",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=28`,
   },
   fetchComedy: {
     title: "Comedy",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=35`,
   },
   fetchRomance: {
     title: "Romance",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=10749`,
   },
   fetchMystery: {
     title: "Mystery",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=9648`,
   },
   fetchThriller: {
     title: "Thriller",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=53`,
   },
   fetchHorror: {
     title: "Horror",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=27`,
   },
-  fetchMisc: {
-    title: "Mischellenous",
-    url: `/trending/all/week?api_key=${API_KEY}&lan=en-US`,
+  fetchScienceFiction: {
+    title: "Sci-Fi",
+    url: `/discover/movie?api_key=61dd2957c20f6dad45d70ee6df520d11&with_genres=878`,
   },
 };
